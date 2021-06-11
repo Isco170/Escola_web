4800d77a4e8d8dc738c915a45340f83f79d17b02
Francisco <franciscodamana@gmail.com>
2021-06-11 08:08:35 +0200

    Configurando chave estrangeira

--- src/main/java/iscim/escola/domain/Estudante.java
+++ src/main/java/iscim/escola/domain/Estudante.java
@@ -2,6 +2,8 @@ package iscim.escola.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @SuppressWarnings("serial")
 @Entity
@@ -17,4 +19,50 @@ public class Estudante extends GenericDomain {
 	
 	@Column(length = 100, nullable = false)
 	private String morada;
+	
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Turma turma;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getApelido() {
+		return apelido;
+	}
+
+	public void setApelido(String apelido) {
+		this.apelido = apelido;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getMorada() {
+		return morada;
+	}
+
+	public void setMorada(String morada) {
+		this.morada = morada;
+	}
+
+	public Turma getTurma() {
+		return turma;
+	}
+
+	public void setTurma(Turma turma) {
+		this.turma = turma;
+	}
+	
+	
 }
