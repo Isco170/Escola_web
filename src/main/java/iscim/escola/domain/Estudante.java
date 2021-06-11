f0dbbf12578b1a1dea5b9985ac81de9e6edfdeed
Francisco <franciscodamana@gmail.com>
2021-06-11 07:58:33 +0200

    Configurando os atributos

--- src/main/java/iscim/escola/domain/Estudante.java
+++ src/main/java/iscim/escola/domain/Estudante.java
@@ -1,13 +1,20 @@
 package iscim.escola.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 
 @SuppressWarnings("serial")
 @Entity
-public class Estudante extends GenericDomain{
-	
+public class Estudante extends GenericDomain {
+	@Column(length = 50, nullable = false)
 	private String nome;
+	
+	@Column(length = 50, nullable = false)
 	private String apelido;
+
+	@Column(length = 20, nullable = false)
 	private String telefone;
+	
+	@Column(length = 100, nullable = false)
 	private String morada;
 }
