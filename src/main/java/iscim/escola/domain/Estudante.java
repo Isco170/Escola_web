147f7d44b4aa93e60bea77ada195b0209635d096
Francisco <franciscodamana@gmail.com>
2021-06-11 07:56:53 +0200

    Criando atributos da classe Estudante

--- src/main/java/iscim/escola/domain/Estudante.java
+++ src/main/java/iscim/escola/domain/Estudante.java
@@ -5,5 +5,9 @@ import javax.persistence.Entity;
 @SuppressWarnings("serial")
 @Entity
 public class Estudante extends GenericDomain{
-
+	
+	private String nome;
+	private String apelido;
+	private String telefone;
+	private String morada;
 }
