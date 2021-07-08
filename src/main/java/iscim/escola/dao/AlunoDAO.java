8347e69f6c9045fed03951bdc8cbd52690960a36
Francisco <franciscodamana@gmail.com>
2021-07-09 00:52:46 +0200

    DAO turma e Estudante. Agora JSP e Servelets

---
+++ src/main/java/iscim/escola/dao/AlunoDAO.java
@@ -0,0 +1,7 @@
+package iscim.escola.dao;
+
+import iscim.escola.domain.Estudante;
+
+public class AlunoDAO extends GenericDAO<Estudante>{
+
+}
---
+++ src/main/java/iscim/escola/dao/TurmaDAO.java
@@ -0,0 +1,7 @@
+package iscim.escola.dao;
+
+import iscim.escola.domain.Turma;
+
+public class TurmaDAO  extends GenericDAO<Turma>{
+
+}
